ff9def9f8057aba34e57fff3e3c4bac14cfaf209
menna-saber-c64 <menna.saber@c64.ae>
2023-05-27 00:08:24 +0300

    Layer Based Archi : Optimize dockerFile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello From Docker Demo project");
+  res.send("Hello From Docker Demo Project!");
 });
 
 app.listen(3000);
